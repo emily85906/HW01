26d48a3cdbb885f9637dec339a28a47024b26d3f
施美妘 <1104137210@office365.kuas.edu.tw>
2018-04-19 16:32:16 +0800

    add0419

---
+++ HW01/Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace HW01.Models
+{
+    public class Order
+    {
+        [DisplayName("訂單編號")]
+        [Required()]
+        public int OrderId { get; set; }
+
+
+        [DisplayName("客戶名稱")]
+        [Required()]
+        public string CustomerName { get; set; }
+
+        [DisplayName("負責員工")]
+        [Required()]
+        public string EmployeeName { get; set; }
+
+        [DisplayName("出貨公司")]
+        public string ShipCompany { get; set; }
+
+        [DisplayName("訂購日期")]
+        public string OrderDate { get; set; }
+
+        [DisplayName("需要日期")]
+        public string ShipDate { get; set; }
+
+        public string RequiredDate { get; set; }
+
+        public int Freight { get; set; }
+
+    }
+}
---
+++ HW01/Models/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW01.Models
+{
+    public class OrderService
+    {
+        public int InsertOrder(Models.Order order)
+        {
+            return 0;
+        }
+
+        public List<Models.Order> GetOrderByCondition(Models.OrderSearchArg arg)
+        {
+            List<Models.Order> result = new List<Order>();
+            result.Add(new Order() { OrderId = 1, CustomerName = "Customer IBVRG", OrderDate = "2006/7/8", ShipDate = "2006/7/12", Freight = 300 });
+            result.Add(new Order() { OrderId = 2, CustomerName = "Customer IBVRG", OrderDate = "2006/7/8", ShipDate = "2006/7/12", Freight = 1500 });
+            result.Add(new Order() { OrderId = 3, CustomerName = "Customer IBVRG", OrderDate = "2006/7/8", ShipDate = "2006/7/12", Freight = 400 });
+            result.Add(new Order() { OrderId = 4, CustomerName = "Customer NRCSK", OrderDate = "2006/7/8", ShipDate = "2006/7/12", Freight = 100 });
+            result.Add(new Order() { OrderId = 5, CustomerName = "Customer NRCSK", OrderDate = "2006/7/8", ShipDate = "2006/7/12", Freight = 300 });
+
+            return result;
+        }
+
+    }
+}
